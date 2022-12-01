8fd436de0de805210656bd5bb644bf384e3ec5d4
SklvSkills <115501824+SklvSkills@users.noreply.github.com>
2022-12-01 12:42:11 +0300

    Create Delegation.swift

---
+++ Fundamental/Delegation/Delegation.swift
@@ -0,0 +1,48 @@
+//
+//  Delegation
+//
+
+protocol DisplayDelegate {
+    func display(_ text: String)
+}
+
+// Message
+
+class Message {
+    let text: String
+    var delegate: DisplayDelegate?
+    
+    init(_ text: String) {
+        self.text = text
+    }
+    
+    func show() {
+        delegate?.display(text)
+    }
+}
+
+// Telegram
+
+class Telegram: Message, DisplayDelegate {
+    override init(_ text: String) {
+        super.init(text)
+        delegate = self
+    }
+    
+    func display(_ text: String) {
+        print(text)
+    }
+}
+
+// Client
+
+class Client {
+    func run() {
+        let telegram = Telegram("Hello, World!")
+        telegram.show()
+    }
+}
+
+// Client().run()
+
+// Hello, World!
