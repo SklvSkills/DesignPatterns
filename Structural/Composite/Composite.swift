50b408f6206e418e6fe72808a6d709d544fbdb49
SklvSkills <115501824+SklvSkills@users.noreply.github.com>
2022-12-02 17:50:35 +0300

    Create Composite.swift

---
+++ Structural/Composite/Composite.swift
@@ -0,0 +1,85 @@
+//
+//  Composite
+//
+
+// Leaf
+
+protocol Component: AnyObject {
+    func operation()
+}
+
+class Leaf: Component {
+    private let number: Int
+    
+    init(_ number: Int) {
+        self.number = number
+    }
+    
+    func operation() {
+        print("Leaf \(number)")
+    }
+}
+
+// Branch
+
+protocol Composite: Component {
+    func add(_ component: Component)
+    func remove(_ component: Component)
+    func getComponents() -> [Component]
+}
+
+class Branch: Composite {
+    private lazy var components = [Component]()
+    
+    func add(_ component: Component) {
+        components.append(component)
+    }
+    
+    func remove(_ component: Component) {
+        while let index = components.firstIndex(where: { $0 === component }) {
+            components.remove(at: index)
+        }
+    }
+    
+    func getComponents() -> [Component] {
+        components
+    }
+    
+    func operation() {
+        components.forEach {
+            $0.operation()
+        }
+    }
+}
+
+// Client
+
+class Client {
+    func run() {
+        let branch1 = Branch()
+        let leaf1 = Leaf(1)
+        let leaf2 = Leaf(2)
+        branch1.add(leaf1)
+        branch1.add(leaf2)
+        
+        let branch2 = Branch()
+        let leaf3 = Leaf(3)
+        let leaf4 = Leaf(4)
+        let leaf5 = Leaf(5)
+        branch2.add(leaf3)
+        branch2.add(leaf4)
+        branch2.add(leaf5)
+        
+        branch1.add(branch2)
+        
+        branch1.operation()
+    }
+}
+
+// Client().run()
+
+// Leaf 1
+// Leaf 2
+// Leaf 3
+// Leaf 4
+// Leaf 5
